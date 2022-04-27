d82d50933ab9d63b6c742c899d73b428347745fc
Rockpods <90806701+Rockpods1@users.noreply.github.com>
2022-04-26 20:34:36 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+httpGet("https://mediaology.com/mediastats/server.php")
